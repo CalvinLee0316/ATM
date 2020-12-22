fd1b2af8fd747123faad36f5cc0e679efd5fc16c
CalvinLee0316 <calee@ucsd.edu>
2020-12-22 12:08:17 +0900

    second commit
    commented, 3 tries for pin number, accounts is a dictionary rather than an array so indices do not need to match account number

--- atm.py
+++ atm.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+#Create an Account class with all necessary attributes and getters and setters
 class Account:
     def __init__(self, accountNum, pin, balance):
         self.accountNum = accountNum
@@ -29,20 +30,28 @@ class Account:
 
     def getBalanceArray(self):
         return self.balance
-    
-accounts = []
+
+#List of Account objects   
+accounts = {}
+#List of all account numbers
 accountNums = []
+#Generate test accounts, accounts and accountNums
+#accounts is a dictionary, so indices do not need to match account numbers
+#Would be populated differently if used by actual bank
+#Would be populated with actual accounts, balances, and pins
 def createTestAccounts():
     for i in range (20):
-        accounts.append(Account(i, 1234+i, 10000+i))
+        accountNumber = i
+        accounts[accountNumber]= Account(accountNumber, 1234+accountNumber, 10000+accountNumber)
         accountNums.append(i)
         accounts[i].addBalance(2000+i)
         accounts[i].addBalance(3000+i)
 
-    
+#function that checks to see if the input account exists
 def accountExists(accountNum):
     return accountNum in accounts
 
+#function that checks to see if the input number is actually a number
 def accountInt(inputAccount):
     try:
         int(inputAccount)
@@ -50,41 +59,61 @@ def accountInt(inputAccount):
     except ValueError:
         return False
 
+#call the function created to generate accounts
 createTestAccounts()
-#TODO main section on console
+
+#main section on console
 while(True):
-   
+    #Put name of bank here
     bankName = 'Calvin Bank'
+    #Welcoming statement, starting screen
     print('#### Welcome to %s, what is your account number? ####' % bankName)
     account_input = input()
+
+    #Check to see if account number is valid
     if accountInt(account_input) != True:
         print('#### Please enter a number ####')
         continue
     if int(account_input) not in accountNums:
         print('#### That account does not exist #####')
         continue
-    print('#### Welcome, account number %s, please enter your pin ####' % account_input)
+    #load the correct account into currAccount
     currAccount = accounts[int(account_input)]
-    try:
-        pin_input = int(input())
-    except ValueError:
-        print('##### Please enter a number ####')
-        continue
-    if (currAccount.correctPin(pin_input)) != True :
-        print('#### Pin is incorrect ####')
-        continue
+    pinTrue = False
 
+    #check if pin is correct, give 3 tries
+    for i in range(3):
+        print('#### Welcome, account number %s, please enter your pin ####' % account_input)
+        #check to see if the pin is valid
+        try:
+            pin_input = int(input())
+            if (currAccount.correctPin(pin_input)) != True :
+                print('#### Pin is incorrect, %d tries left ####' % (2-i))
+            else:
+                pinTrue = True
+                break
+        except ValueError:
+            print('##### Please enter a number ####')
+            print('#### %d tries left ####' % (2-i))
+            pinTrue = False
+
+    #if pin was not matched go back to beginning screen
+    if pinTrue == False:
+        continue
+    
     print('#### Which balance would you like access? ####')
+    #List all balance options in given account
     for i in range(0, len(currAccount.getBalanceArray())):
         print('Balance %d' % (i+1))
 
-  
+    
     try:
         balanceNum = int(input())
     except ValueError:
         print('#### Please enter a number ####')
         continue
         
+    #check to see if balance number is valid
     if int(balanceNum) > len(currAccount.getBalanceArray()) | int(balanceNum)<1:
         print('Invalid Balance Number')
         continue
@@ -94,19 +123,24 @@ while(True):
     print('#### 2. Deposit                ####')
     print('#### 3. Withdraw               ####')
     print('#### 4. Exit                   ####')
+    #check to see if service input was a number
     try:
         service = int(input())
     except ValueError:
         print('Invalid service number')
         continue
+
+    #check to see if service input was valid
     if service > 4 | service <1:
         print('#### Invalid service number, please input a number between 1 and 4 ####')
         continue
+    #View balance
     if service == 1:
         print('#### Current Balance : $%d ####' % (currAccount.selectBalance(balanceNum)))
         print('#### Press enter to exit ####')
         input()
         break
+    #Deposit
     elif service == 2:
         print('#### How much would you like to deposit? ####')
         try:
@@ -123,6 +157,7 @@ while(True):
             print('#### Press enter to exit')
             input()
             break
+    #Withdraw
     elif service == 3:
         print('#### How much would you like to withdraw? ####')
         try:
@@ -143,8 +178,10 @@ while(True):
             print('#### Press enter to exit')
             input()
             break
+    #Exit
     else:
         break
+#Exitting message
 print('#### Thank you for using %s, come again! ####' % bankName)
         
         
